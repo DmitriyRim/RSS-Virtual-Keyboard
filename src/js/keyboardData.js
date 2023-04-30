1bcb2db91ac95502e2ef6896a6d9540d45017a5f
Rimfis <dimitri.rim@yandex.ru>
2023-04-30 14:36:55 +0300

    feat: add data for keyboard

---
+++ src/js/keyboardData.js
@@ -0,0 +1,355 @@
+const dataKeyboard = [
+    [
+        {
+            en: {
+                value: "`",
+                altValue : "~",
+            },
+            ru: {
+                value: "ё",
+            },
+            className: "keyboard__btn"
+        },
+        {
+            value: "1",
+            altValue : "!",
+            className: "keyboard__btn"
+        },
+        {
+            value: "2",
+            ru: {altValue : "\""},
+            en: {altValue : "@"},
+            className: "keyboard__btn"
+        },
+        {
+            value: "3",
+            ru: {altValue : "№"},
+            en: {altValue : "#"},
+            className: "keyboard__btn"
+        },
+        {
+            value: "4",
+            ru: {altValue : ";"},
+            en: {altValue : "$"},
+            className: "keyboard__btn"
+        },
+        {
+            value: "5",
+            altValue : "%",
+            className: "keyboard__btn"
+        },
+        {
+            value: "6",
+            ru: {altValue : ":"},
+            en: {altValue : "^"},
+            className: "keyboard__btn"
+        },
+        {
+            value: "7",
+            ru: {altValue : "?"},
+            en: {altValue : "&"},
+            className: "keyboard__btn"
+        },
+        {
+            value: "8",
+            altValue : "*",
+            className: "keyboard__btn"
+        },
+        {
+            value: "9",
+            altValue : "(",
+            className: "keyboard__btn"
+        },
+        {
+            value: "0",
+            altValue : ")",
+            className: "keyboard__btn"
+        },
+        {
+            value: "-",
+            altValue : "_",
+            className: "keyboard__btn"
+        },
+        {
+            value: "=",
+            altValue : "+",
+            className: "keyboard__btn"
+        },
+        {
+            value: "Backspace",
+            className: "keyboard__btn keyboard__backspace keyboard__btn-action"
+        },
+    ],
+    [
+        {
+            value: "Tab",
+            className: "keyboard__btn keyboard__backspace keyboard__btn-action"
+        },
+        {
+            en: {value: "q"},
+            ru: {value: "й"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "w"},
+            ru: {value: "ц"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "r"},
+            ru: {value: "к"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "t"},
+            ru: {value: "е"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "y"},
+            ru: {value: "н"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "u"},
+            ru: {value: "г"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "i"},
+            ru: {value: "ш"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "o"},
+            ru: {value: "щ"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "p"},
+            ru: {value: "з"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "q"},
+            ru: {value: "й"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {
+                value: "[",
+                altValue: "{"
+            },
+            ru: {
+                value: "х",
+            },
+            className: "keyboard__btn"
+        },
+        {
+            en: {
+                value: "]",
+                altValue: "}"
+            },
+            ru: {
+                value: "ъ",
+            },
+            className: "keyboard__btn"
+        },
+        {
+            value: "Del",
+            className: "keyboard__btn keyboard__backspace keyboard__btn-action"
+        },
+    ],
+    [
+        {
+            value: "CapsLock",
+            className: "keyboard__btn keyboard__backspace keyboard__btn-action"
+        },
+        {
+            en: {value: "a"},
+            ru: {value: "ф"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "s"},
+            ru: {value: "ы"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "d"},
+            ru: {value: "в"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "f"},
+            ru: {value: "а"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "g"},
+            ru: {value: "п"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "h"},
+            ru: {value: "р"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "j"},
+            ru: {value: "о"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "k"},
+            ru: {value: "л"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "l"},
+            ru: {value: "д"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {
+                value: ";",
+                altValue: ":",    
+            },
+            ru: {value: "ж"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {
+                value: "'",
+                altValue: "\"",    
+            },
+            ru: {value: "э"},
+            className: "keyboard__btn"
+        },
+        {
+            value: "\\",
+            altValue: "|",
+            className: "keyboard__btn"
+        },
+        {
+            value: "Enter",
+            className: "keyboard__btn keyboard__backspace keyboard__btn-action"
+        },
+    ],
+    [
+        {
+            value: "Shift",
+            className: "keyboard__btn keyboard__backspace keyboard__btn-action"
+        },
+        {
+            en: {value: "z"},
+            ru: {value: "я"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "x"},
+            ru: {value: "ч"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "c"},
+            ru: {value: "с"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "v"},
+            ru: {value: "м"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "b"},
+            ru: {value: "и"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "n"},
+            ru: {value: "т"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {value: "m"},
+            ru: {value: "ь"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {
+                value: ",",
+                altValue: "<",
+            },
+            ru: {value: "б"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {
+                value: ".",
+                altValue: ">",
+            },
+            ru: {value: "ю"},
+            className: "keyboard__btn"
+        },
+        {
+            en: {
+                value: "/",
+                altValue: "?",
+            },
+            ru: {
+                value: ".",
+                altValue: ",",
+            },
+            className: "keyboard__btn"
+        },
+        {
+            value: "↑",
+            className: "keyboard__btn keyboard__backspace"
+        },
+        {
+            value: "Shift",
+            className: "keyboard__btn keyboard__backspace keyboard__btn-action"
+        },
+    ],
+    [
+        {
+            value: "Ctrl",
+            className: "keyboard__btn keyboard__backspace keyboard__btn-action"
+        },
+        {
+            value: "Win",
+            className: "keyboard__btn keyboard__backspace keyboard__btn-action"
+        },
+        {
+            value: "Alt",
+            className: "keyboard__btn keyboard__backspace keyboard__btn-action"
+        },
+        {
+            value: " ",
+            className: "keyboard__btn keyboard__backspace keyboard__btn_space"
+        },
+        {
+            value: "Alt",
+            className: "keyboard__btn keyboard__backspace keyboard__btn-action"
+        },
+        {
+            value: "←",
+            className: "keyboard__btn keyboard__backspace"
+        },
+        {
+            value: "↓",
+            className: "keyboard__btn keyboard__backspace"
+        },
+        {
+            value: "→",
+            className: "keyboard__btn keyboard__backspace"
+        },
+        {
+            value: "Ctrl",
+            className: "keyboard__btn keyboard__backspace keyboard__btn-action"
+        },
+    ]
+];
+
+export default dataKeyboard;
